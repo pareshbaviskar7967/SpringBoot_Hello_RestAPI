6384cfdee1d8d00470f0827c7a84a91f1c36ea9a
pareshbaviskar7967 <pareshbaviskar7967@gmail.com>
2021-08-09 14:59:36 +0530

    Solution for UC-2

--- Hello_Using_Rest_API/src/main/java/com/example/hello_using_rest_api/controller/HelloRestAPI.java
+++ Hello_Using_Rest_API/src/main/java/com/example/hello_using_rest_api/controller/HelloRestAPI.java
@@ -1,6 +1,8 @@
 package com.example.hello_using_rest_api.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +11,11 @@ public class HelloRestAPI {
     @RequestMapping(value = {"", "/", "/home"})
     public String Controller() {
         return "Hello  from BridgeLabz";
+    }
+    //http://localhost:8080/query?name=paresh
+    @RequestMapping(value = {"/query"}, method = RequestMethod.GET)
+    public String sayHello(@RequestParam(value = "name") String name)
+    {
+        return "Hello " + name + "!"+"How r u";
     }
 }
